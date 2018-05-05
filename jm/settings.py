1b1f0204dd337b3d191277c298d88db2e78e07bf
saviolinski <saviolinski@gmail.com>
2018-05-05 18:42:32 +0200

    pepea5wd3

--- jm/settings.py
+++ jm/settings.py
@@ -41,6 +41,12 @@ DATABASES = {
 ALLOWED_HOSTS = ['127.0.0.1', '0.0.0.0', 'localhost', '.herokuapp.com']
 # Application definition
 
+EMAIL_HOST = config('EMAIL_HOST', default='localhost')
+EMAIL_PORT = config('EMAIL_PORT', default=25, cast=int)
+EMAIL_HOST_PASSWORD = config('EMAIL_HOST_PASSWORD', default='')
+EMAIL_HOST_USER = config('EMAIL_HOST_USER', default='')
+EMAIL_USE_TLS = config('EMAIL_USE_TLS', default=False, cast=bool)
+
 INSTALLED_APPS = [
     'django.contrib.admin',
     'django.contrib.auth',
