32ed5bee24d2be7d7add5cebb44ff533189d196d
saviolinski <saviolinski@gmail.com>
2018-05-05 18:49:51 +0200

    pepea5wd3d

--- jm/settings.py
+++ jm/settings.py
@@ -12,7 +12,7 @@ https://docs.djangoproject.com/en/1.11/ref/settings/
 
 import os
 import dj_database_url
-from decouple import config
+from decouple import config, Csv
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
@@ -38,7 +38,8 @@ DATABASES = {
     )
 }
 
-ALLOWED_HOSTS = ['127.0.0.1', '0.0.0.0', 'localhost', '.herokuapp.com']
+ALLOWED_HOSTS = config('ALLOWED_HOSTS', cast=Csv())
+# ALLOWED_HOSTS = ['127.0.0.1', '0.0.0.0', 'localhost', '.herokuapp.com']
 # Application definition
 
 EMAIL_HOST = config('EMAIL_HOST', default='localhost')
