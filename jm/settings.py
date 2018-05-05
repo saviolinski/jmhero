8f5491f8e802671226fd330ba00dbce39cf134c4
saviolinski <saviolinski@gmail.com>
2018-05-05 17:04:04 +0200

    pep3

--- jm/settings.py
+++ jm/settings.py
@@ -26,7 +26,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # SECURITY WARNING: don't run with debug turned on in production!
 # DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1', '0.0.0.0', 'localhost', '.herokuapp.com']
+
 
 
 
@@ -38,6 +38,7 @@ DATABASES = {
     )
 }
 
+ALLOWED_HOSTS = ['127.0.0.1', '0.0.0.0', 'localhost', '.herokuapp.com']
 # Application definition
 
 INSTALLED_APPS = [
